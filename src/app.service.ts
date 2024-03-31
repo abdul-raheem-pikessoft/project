e14348eb850090ae8b2d95b0b41d34994c9807cb
abdul-raheem-pikessoft <abdul.raheem@pikessoft.com>
2024-03-31 22:09:19 +0500

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  sampleApi(): string {
+    return '<h1>Sample project is run on EC2  Instance</h1>';
   }
 }
